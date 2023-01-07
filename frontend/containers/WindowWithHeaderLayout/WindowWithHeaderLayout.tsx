16dbfe0e5871d75acd4e23f3c1e2f9164771242c
H1ako <nikita-sobolev-wd@yandex.ru>
2023-01-07 13:36:53 +0400

    fixed bug with header open in active header window

--- frontend/containers/WindowWithHeaderLayout/WindowWithHeaderLayout.tsx
+++ frontend/containers/WindowWithHeaderLayout/WindowWithHeaderLayout.tsx
@@ -4,6 +4,8 @@ import React from 'react'
 import InnerBlockHeading from "../../components/InnerBlockHeading/InnerBlockHeading";
 // styles and icons
 import styles from './WindowWithHeaderLayout.module.scss';
+import {useRecoilState, useSetRecoilState} from "recoil";
+import {headerBlockStateAtom} from "../../recoilAtoms/headerAtoms";
 
 interface WindowWithHeaderLayoutProps {
   className?: string,
@@ -15,10 +17,24 @@ interface WindowWithHeaderLayoutProps {
 
 function WindowWithHeaderLayout(
   {children, ToggleButton, heading='', className='', wrapperClassName=''}: WindowWithHeaderLayoutProps) {
+  const [ isHeaderBlocked, setHeaderBlock ] = useRecoilState(headerBlockStateAtom)
+  const setIsHeaderActive = useSetRecoilState(headerBlockStateAtom)
   const [ isActive, setActivity ] = React.useState<boolean>(false)
   
   const toggleMenu = () => {
+    setIsHeaderActive(true)
+    toggleHeaderBlockOnActivity()
+    
     setActivity(state => !state)
+  }
+  
+  const toggleHeaderBlockOnActivity = () => {
+    if (isActive) {
+      setHeaderBlock(false)
+    }
+    else {
+      setHeaderBlock(true)
+    }
   }
   
   return (
