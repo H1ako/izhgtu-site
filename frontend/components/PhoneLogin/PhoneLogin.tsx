dd8b7fc659ea5e20056149db86a14cb5fb8f9f2f
H1ako <nikita-sobolev-wd@yandex.ru>
2023-02-14 19:16:57 +0400

    updated frontend for phone login

--- frontend/components/PhoneLogin/PhoneLogin.tsx
+++ frontend/components/PhoneLogin/PhoneLogin.tsx
@@ -10,13 +10,21 @@ interface PhoneLoginProps {
   children?: React.ReactNode,
   onSend?: (phone: string) => void,
   onCodeSubmit?: (code: string) => void,
+  onCheckSuccess?: (token: string) => void,
 }
 
-function PhoneLogin({className, children, onSend, onCodeSubmit}: PhoneLoginProps) {
+interface CodeCheckSuccessScreenProps {
+  className?: string,
+}
+
+function PhoneLogin({className, children, onSend, onCodeSubmit, onCheckSuccess}: PhoneLoginProps) {
   const [ codeSent, setCodeSent ] = React.useState<boolean>(false)
+  const [ codeCheckSent, setCodeCheckSent ] = React.useState<boolean>(false)
   const [ error, setError ] = React.useState<string | null>(null)
-  const inputRef = React.useRef(null)
+  const inputRef = React.useRef<any>(null)
   const [ phone, setPhone ] = React.useState<string>('')
+  const [ codeInputValue, setCodeInputValue ] = React.useState<string>('')
+  const [ codeCheckSuccess, setCodeCheckSuccess ] = React.useState<boolean>(false)
   
   const sendCode = () => {
     fetch('/api/auth/passwordless/mobile', {
@@ -35,8 +43,54 @@ function PhoneLogin({className, children, onSend, onCodeSubmit}: PhoneLoginProps
     setCodeSent(true)
   }
   
+  const sendCodeAgain = () => {
+    if (codeCheckSent || codeCheckSuccess) return
+    
+    sendCode()
+  }
+  
   const getMobile = () => {
     return `+${phone}`
+  }
+  
+  const sendCodeForCheck = (code: string) => {
+    fetch('/api/auth/passwordless/token/', {
+      method: 'POST',
+      body: JSON.stringify({
+        mobile: getMobile(),
+        token: code
+      }),
+      headers: {
+        mode: 'cors',
+        'Content-Type': 'application/json',
+        'X-CSRFToken': Cookies.get('csrftoken') || ''
+      }
+    })
+      .then(res => {
+        if (res.status === 200) {
+          const data = res.json()
+            .then(data => {
+              const token = data.token
+          
+              setCodeCheckSuccess(true)
+              onCheckSuccess && onCheckSuccess(token)
+            })
+        } else {
+          setError(res.statusText)
+          setCodeCheckSent(false)
+        }
+      })
+  }
+  
+  React.useEffect(() => {
+    if (codeInputValue.length !== 6 || codeCheckSent || codeCheckSuccess) return
+    
+    sendCodeForCheck(codeInputValue)
+    setCodeCheckSent(true)
+  }, [codeInputValue])
+  
+  if (codeCheckSuccess) {
+    return <CodeCheckSuccessScreen className={className} />
   }
   
   return (
@@ -53,6 +107,8 @@ function PhoneLogin({className, children, onSend, onCodeSubmit}: PhoneLoginProps
             name='sms-code-field'
             fields={6}
             type='text'
+            value={codeInputValue}
+            onChange={value => setCodeInputValue(value)}
             inputMode="full-width-latin"
           />
           { error &&
@@ -77,6 +133,14 @@ function PhoneLogin({className, children, onSend, onCodeSubmit}: PhoneLoginProps
         </>
       }
       {children}
+    </div>
+  )
+}
+
+const CodeCheckSuccessScreen = ({className}: CodeCheckSuccessScreenProps) => {
+  return (
+    <div className={`${styles.phoneLogin} ${className}`}>
+      <h1 className={styles.phoneLogin__statusMessage}>Код подтверждения успешно введен</h1>
     </div>
   )
 }
